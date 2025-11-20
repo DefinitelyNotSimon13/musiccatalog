ae6a5bf4e0fbeee940cf6001ddfd28b2d89d1d49
benoeckl <ben@oeckl.com>
2025-11-20 14:07:30 +0100

    add logic to mongo adapter

--- backend/src/main/java/org/uni/music/adapter/TrackMongoAdapter.java
+++ backend/src/main/java/org/uni/music/adapter/TrackMongoAdapter.java
@@ -1,30 +1,54 @@
 package org.uni.music.adapter;
 
+import org.springframework.stereotype.Component;
 import org.uni.music.model.Track;
+import org.uni.music.repository.MusicMongoRepository;
 
 import java.util.List;
 
+@Component
 public class TrackMongoAdapter implements TrackAdapter {
 
-    // TODO Implement me
+    private final MusicMongoRepository repository;
 
+    public TrackMongoAdapter(MusicMongoRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
     public List<String> getTracks() {
-        return List.of();
+        return repository.findAll().stream()
+                .map(Track::getId)
+                .toList();
     }
 
+    @Override
     public Track getTrack(String id) {
-        return new Track();
+        return repository.findById(id).orElse(null);
     }
 
+    @Override
     public boolean createTrack(Track track) {
+        repository.save(track);
         return true;
     }
 
+    @Override
     public boolean updateTrack(String id, Track track) {
+        if (!repository.existsById(id)) {
+            return false;
+        }
+        track.setId(id);
+        repository.save(track);
         return true;
     }
 
+    @Override
     public boolean deleteTrack(String id) {
+        if (!repository.existsById(id)) {
+            return false;
+        }
+        repository.deleteById(id);
         return true;
     }
 }
