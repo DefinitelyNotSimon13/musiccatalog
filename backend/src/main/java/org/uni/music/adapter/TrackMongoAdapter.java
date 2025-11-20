149e45d43c21e69938ffb6355850990ce7794936
DosKobold <hanspeter56@t-online.de>
2025-11-20 13:47:05 +0100

    feat: add mongo adapter

---
+++ backend/src/main/java/org/uni/music/adapter/TrackMongoAdapter.java
@@ -0,0 +1,26 @@
+package org.uni.music.adapter;
+
+import org.uni.music.model.Track;
+
+import java.util.List;
+
+public class TrackMongoAdapter implements TrackAdapter {
+
+    // TODO Implement me
+
+    public List<Track> getTracks() {
+        return List.of();
+    }
+
+    public List<Track> getTrack(String id) {
+        return List.of();
+    }
+
+    public List<Track> updateTrack(String id, Track track) {
+        return List.of();
+    }
+
+    public List<Track> deleteTrack(String id) {
+        return List.of();
+    }
+}
