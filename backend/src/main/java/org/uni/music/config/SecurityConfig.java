6dbca0bb4fb22151b7ca0d310d586a5f3017912d
Simon Blum <simon21.blum@gmail.com>
2025-11-27 13:41:01 +0100

    Add more auth stuff

--- backend/src/main/java/org/uni/music/config/SecurityConfig.java
+++ backend/src/main/java/org/uni/music/config/SecurityConfig.java
@@ -16,6 +16,7 @@ import org.springframework.security.oauth2.client.registration.ClientRegistratio
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.uni.music.util.JwtConverter;
 //import org.uni.music.security.JwtConverter;
 
 @Configuration
@@ -23,37 +24,28 @@ import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 @EnableMethodSecurity
 @AllArgsConstructor
 public class SecurityConfig {
-//    private final JwtConverter jwtConverter;
+    private final JwtConverter jwtConverter;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, ClientRegistrationRepository clientRegistrationRepository) throws Exception {
         http
-                .authorizeHttpRequests((requests) -> requests
-//                        .requestMatchers("/").permitAll()
-                        .anyRequest().permitAll()
+                .authorizeHttpRequests(auth -> auth
+                        // allow swagger if you want
+                        .requestMatchers("/docs/**", "/v3/api-docs/**").permitAll()
+                        .anyRequest().authenticated()
                 )
-//                .oauth2ResourceServer(
-//                        (oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtConverter)))
-//                )
-//                .logout((logout) -> {
-//                    var logoutSuccessHandler = new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
-//                    logoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}/");
-//                    logout.logoutSuccessHandler(logoutSuccessHandler);
-//                })
-//                .oauth2Login(Customizer.withDefaults())
-//                .oauth2Client(Customizer.withDefaults())
+                .oauth2ResourceServer(
+                        (oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtConverter)))
+                )
+                .oauth2Login(Customizer.withDefaults())
+                .oauth2Client(Customizer.withDefaults())
+                .logout(logout -> {
+                    var logoutSuccessHandler =
+                            new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+                    logoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}/");
+                    logout.logoutSuccessHandler(logoutSuccessHandler);
+                });
         ;
         return http.build();
     }
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//        UserDetails user =
-//                User.withDefaultPasswordEncoder()
-//                        .username("user")
-//                        .password("password")
-//                        .roles("USER")
-//                        .build();
-//
-//        return new InMemoryUserDetailsManager(user);
-//    }
 }
--- backend/src/main/java/org/uni/music/repository/ArtistRepository.java
+++ backend/src/main/java/org/uni/music/repository/ArtistRepository.java
@@ -3,9 +3,7 @@ package org.uni.music.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import org.uni.music.model.Artist;
-import org.uni.music.model.Track;
 
-import java.util.List;
 import java.util.UUID;
 
 @Repository
